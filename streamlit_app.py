50d723aefdcf4f8bddecc0660294e0dd40546a65
dk8281 <digvijaykadam2096@gmail.com>
2025-04-24 11:34:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,15 @@ st.dataframe(city_country_counts.sort_values(by="RestaurantCount", ascending=Fal
 
 # ----- Restaurant Openings Over Time
 st.subheader("📆 Restaurants Opened Over Time")
-df['Datekey_Opening'] = pd.to_datetime(df['Datekey_Opening'], errors='coerce')
+df['Datekey_Opening'] = pd.to_datetime(df['Datekey_Opening'])
+
 df['Year'] = df['Datekey_Opening'].dt.year
-openings_by_year = df.groupby('Year')['RestaurantID'].nunique().reset_index(name='RestaurantCount')
+df['Quarter'] = df['Datekey_Opening'].dt.quarter
+df['Month'] = df['Datekey_Opening'].dt.month
+
+openings_by_time = df.groupby(['Year', 'Quarter', 'Month'])['RestaurantID'].nunique().reset_index(name='RestaurantCount')
+print(openings_by_time)
+
 
 fig, ax = plt.subplots()
 ax.plot(openings_by_year['Year'], openings_by_year['RestaurantCount'], marker='o')
@@ -50,3 +56,28 @@ fig2, ax2 = plt.subplots()
 ax2.pie(rating_counts['RestaurantCount'], labels=rating_counts['RatingBucket'], autopct='%1.1f%%')
 ax2.set_title("Distribution of Ratings")
 st.pyplot(fig2)
+
+# ----- Price Distribution
+st.subheader("💲 Price Distribution")
+price_bins = [0, 200, 400, 600, 800, 10000]
+price_labels = ['0-200', '201-400', '401-600', '601-800', '800+']
+df['PriceBucket'] = pd.cut(df['Average_Cost_for_two'], bins=price_bins, labels=price_labels, include_lowest=True)
+
+price_counts = df['PriceBucket'].value_counts().sort_index().reset_index(name='RestaurantCount')
+price_counts.columns = ['PriceBucket', 'RestaurantCount']
+print(price_counts)
+
+fig2, ax2 = plt.subplots()
+ax2.pie(Price_counts['RestaurantCount'], labels=rating_counts['PriceBucket'], autopct='%1.1f%%')
+ax2.set_title("Distribution of Price")
+st.pyplot(fig2)
+
+# ----- Percentage of Restaurants with Table Booking
+st.subheader("🗃️ Percentage of Restaurants with Table Booking")
+table_booking_percent = df['Has_Table_booking'].value_counts(normalize=True) * 100
+print(table_booking_percent.astype(str) + '%')
+
+# ----- Percentage of Restaurants with Online Delivery
+st.subheader("📲 Percentage of Restaurants with Online Delivery")
+online_delivery_percent = df['Has_Online_delivery'].value_counts(normalize=True) * 100
+print(online_delivery_percent.astype(str) + '%')
